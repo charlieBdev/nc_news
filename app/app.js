6a198a6b03f5d29bbeb49ca233532421740003dc
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-06-27 15:23:25 +0100

    complete ex5

--- app/app.js
+++ app/app.js
@@ -4,13 +4,15 @@ const { handleCustomErrors, handleServerErrors } = require("./errors/index")
 
 const { getAllTopics } = require("../app/controllers/topics.controller")
 const { getEndpoints } = require("./controllers/endpoints.controller")
-const { getArticleById } = require("./controllers/articles.controller")
+const { getArticleById, getAllArticles } = require("./controllers/articles.controller")
 
 const app = express()
 
 app.get("/api/topics", getAllTopics)
 app.get("/api", getEndpoints)
 app.get("/api/articles/:article_id", getArticleById)
+app.get('/api/articles', getAllArticles)
+
 
 app.get("*", (_, res) => {
   res.status(404).send({ msg: "Not found" })
--- app/models/articles.model.js
+++ app/models/articles.model.js
@@ -8,8 +8,8 @@ exports.selectArticleById = (article_id) => {
   }
   
   const queryString = `
-        SELECT * FROM articles WHERE article_id = $1;
-    `
+    SELECT * FROM articles WHERE article_id = $1;
+  `
   return db.query(queryString, [article_id]).then(({ rows }) => {
     if (rows.length === 0) {
       return Promise.reject({ status: 404, msg: "Not found" })
@@ -17,4 +17,14 @@ exports.selectArticleById = (article_id) => {
       return rows[0]
     }
   })
+}
+
+exports.selectAllArticles = () => {    
+    const queryStr = `
+    SELECT author(articles), title(articles), article_id(articles), topic(articles), created_at(articles), votes(articles), article_img_url(articles), SUM(article_id(articles)) AS comment_count FROM articles LEFT JOIN comments ON article_id(articles) = article_id(comments) GROUP BY article_id(articles) ORDER BY created_at(articles) DESC;
+    `
+    return db.query(queryStr)
+        .then(({ rows }) => {
+            return rows
+        })
 }
