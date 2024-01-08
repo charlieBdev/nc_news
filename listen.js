44e84f951f5209aae3b2a96bf46cec7654915e7e
bigspambot <67501435+bigspambot@users.noreply.github.com>
2024-01-08 12:33:39 +0000

    add cron job

--- listen.js
+++ listen.js
@@ -1,6 +1,11 @@
-const app = require('./app/app')
-const { PORT = 9090 } = process.env
+const app = require('./app/app');
+const { default: cronJob } = require('./pinger/pinger');
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}...`);
+	console.log(`Listening on ${PORT}...`);
 });
+
+if (process.env.NODE_ENV === 'production') {
+	cronJob.start();
+}
---
+++ pinger/pinger.js
@@ -0,0 +1,12 @@
+import http from 'http';
+import cron from 'node-cron';
+
+const cronJob = () => {
+	return cron.schedule('*/14 * * * *', () => {
+		http.get('https://nc-news-qvv1.onrender.com', () => {
+			console.log('Pinged!');
+		});
+	});
+};
+
+export default cronJob;
